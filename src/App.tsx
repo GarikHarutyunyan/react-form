5011d3d351fd13f68fd7d80423c8377eeac6c0f7
Garik <garik.harutyunian@gmail.com>
2024-05-27 18:52:14 +0400

    init vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Form } from './components/Form'
+
+function App() {
+
+  return (
+    <>
+      <Form config={{}} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IFormProps {
+  config: any;
+}
+
+const Form: React.FC<IFormProps> = () => {
+  return <div>{'Form'}</div>;
+};
+
+export {Form};
